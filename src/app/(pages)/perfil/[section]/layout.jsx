4a5aa56015eebd86db99ed4c55d50e77e89899fb
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-04-24 22:38:57 -0300

    finish mobile version of Profile Datos

--- src/app/(pages)/perfil/[section]/layout.jsx
+++ src/app/(pages)/perfil/[section]/layout.jsx
@@ -10,13 +10,13 @@ export default async function PerfilLayout({ children, params }) {
   const link_style = "inline-flex justify-center items-center gap-x-4";
 
   return (
-    <main className="max-w-7xl mx-auto grid grid-cols-[400px_auto] gap-x-5 mb-12">
+    <main className="max-w-7xl mx-auto grid grid-rows-[auto_1fr] lg:grid-rows-1 lg:grid-cols-[300px_auto] gap-5 mb-12 px-2">
       <nav id="menu-perfil" className="p-6 bg-white rounded-2xl">
-        <h1 className="text-3xl text-main font-medium mb-4 pb-6 border-b-1 border-main_stroke">
+        <h1 className="text-2xl lg:text-3xl text-main font-medium mb-4 pb-2 lg:pb-6 border-b-1 border-main_stroke">
           Mi perfil
         </h1>
         <ul>
-          <li className="text-lg mb-4">
+          <li className="text-base lg:text-lg mb-2 lg:mb-4">
             <Link
               href="/perfil/datos"
               className={`${link_style} ${
@@ -28,7 +28,7 @@ export default async function PerfilLayout({ children, params }) {
               <span className="pt-0.5">Mis datos</span>
             </Link>
           </li>
-          <li className="text-lg mb-4">
+          <li className="text-base lg:text-lg mb-2 lg:mb-4">
             <Link
               href="/perfil/compras"
               className={`${link_style} ${
@@ -40,7 +40,7 @@ export default async function PerfilLayout({ children, params }) {
               <span className="pt-0.5">Mis compras y pedidos</span>
             </Link>
           </li>
-          <li className="text-lg mb-4">
+          <li className="text-base lg:text-lg mb-2 lg:mb-4">
             <Link
               href="/perfil/favoritos"
               className={`${link_style} ${
@@ -54,6 +54,7 @@ export default async function PerfilLayout({ children, params }) {
           </li>
         </ul>
       </nav>
+
       {children}
     </main>
   );
